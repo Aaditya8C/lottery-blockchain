e1691a544ca11d9e1fa1d4e8e2d618d5d93d2cf4
Aaditya8C <aadityajp0419@gmail.com>
2024-11-05 12:35:52 +0530

    constract_address_added

--- frontend/components/utils/constants.js
+++ frontend/components/utils/constants.js
@@ -1,3 +1,3 @@
 import abi from "./Transactions.json";
-export const contractAddress = "0xdb99b63519e36f2dfd34ff1a13d65de1392552c0"; // get it from remix ide log
+export const contractAddress = "0x346ac4acd02f5c4c19108bcb35545eb141e93f64"; // get it from remix ide log
 export const contractAbi = abi;
